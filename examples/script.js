65a57a4d3297e8da10053e98a1e1bd19767290b1
thien.hang <me@thienhang.com>
2025-02-21 14:46:05 +0700

    chore: update model

---
+++ examples/script.js
@@ -0,0 +1,314 @@
+let loader, errorMessage;
+
+function showLoading() {
+    loader.style.display = 'inline-block';
+    errorMessage.style.display = 'none';
+}
+
+function hideLoading() {
+    loader.style.display = 'none';
+}
+
+function hideError() {
+    errorMessage.style.display = 'none';
+}
+
+function showError(message) {
+    errorMessage.style.display = 'inline-block';
+    errorMessage.innerText = message;
+    loader.style.display = 'none';
+}
+
+const baseURL = ``;
+const modelConfig = {};
+
+const speakerFiles = {};
+
+let allModels = [];
+
+async function loadModelInfo() {
+    let modelLanguages, modelSpeakers;
+    {
+    const response = await fetch(`/models/languages`);
+    if(response.ok) {
+        modelLanguages = await response.json();
+    }
+    }
+    {
+    const response = await fetch(`/models/speakers`);
+    if(response.ok) {
+        modelSpeakers = await response.json();
+    }
+    }
+    for(const [name, languages] of Object.entries(modelLanguages)) {
+    if(!modelConfig[name]) {
+        modelConfig[name] = { name };
+    }
+    modelConfig[name].languages = languages;
+    }
+    for(const [name, speakers] of Object.entries(modelSpeakers)) {
+    if(!modelConfig[name]) {
+        modelConfig[name] = { name };
+    }
+    modelConfig[name].speakers = speakers;
+    }
+    // console.log(modelLanguages);
+    // console.log(modelSpeakers);
+    console.log(modelConfig);
+    // sort
+}
+
+async function loadAllModelsList() {
+    const response = await fetch(`/models/all`);
+    if(response.ok) {
+    allModels = await response.json();
+    }
+}
+
+function selectSpeaker(event) {
+    const fileSelector = document.querySelector('#speaker-file-selector')
+    fileSelector.click();
+}
+
+async function speakerFileSelected(event) {
+    speakerFile = event.target.files[0];
+    // const status = document.querySelector('#status')
+    // reset file selector: https://stackoverflow.com/a/35323290
+    event.target.value = ''
+    if(!/safari/i.test(navigator.userAgent)){
+    event.target.type = ''
+    event.target.type = 'file'
+    }
+
+    if(!speakerFile) {
+    return;
+    }
+
+    console.log('speaker file:', speakerFile);
+
+    addExternalSpeaker(speakerFile);
+}
+
+async function clear() {
+    const result = document.querySelector(`div#results`);
+    result.innerHTML = '';
+    document.querySelector(`#results-title`).style.display = 'none';
+}
+
+async function generate() {
+    const modelName = document.querySelector(`select#model`).value;
+    const text = document.querySelector(`textarea#text`).value;
+    const language = document.querySelector(`select#language`).value;
+    const speaker = document.querySelector(`select#speaker`).value;
+    console.log(modelName, language, speaker);
+    console.log(text);
+
+    const headers = { 'Accept': 'audio/*' };
+    const formData = new FormData();
+    // formData.append('model_name', 'tts_models/multilingual/multi-dataset/your_tts');
+    formData.append('text', text);
+    formData.append('language', language);
+    if(speaker.startsWith('--')) {
+    formData.append('speaker_wav', speakerFiles[speaker]);
+    } else {
+    formData.append('speaker', speaker);
+    }
+    // formData.append('download', 'on');
+    const body = formData;
+    try {
+    showLoading();
+
+    const response = await fetch(`/models/${modelName}/generate`, { method: 'POST', body, headers });
+    if(!response.ok) {
+        console.log(response);
+        // const data = await response.json();
+        // data.detail
+        showError(`Response ${response.status}: ${response.statusText}`);
+        hideLoading();
+        return;
+    }
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+
+
+    const result = document.querySelector(`div#results`);
+    // result.innerHTML = '';
+
+    const container = document.createElement('div');
+
+    container.classList.add('result');
+
+    const infoContainer = document.createElement('div');
+
+    const now = new Date();
+    const nowstring = now.toISOString();
+    const datetime = nowstring.replace('T', ' ').replace('Z', ' UTC');
+    const speakerName = speaker.startsWith('--') ? speakerFiles[speaker].name : speaker || 'none';
+
+    const info = { 'Date-Time': datetime, Model: modelName, Language: language, Speaker: speakerName, Text: text };
+    for(const [title, value] of Object.entries(info)) {
+        const div = document.createElement('div');
+
+        const titleSpan = document.createElement('span');
+        titleSpan.classList.add('info-title');
+        titleSpan.innerText = `${title}: `;
+        div.appendChild(titleSpan);
+
+        const valueSpan = document.createElement('span');
+        valueSpan.classList.add('info-value');
+        valueSpan.innerText = value;
+        div.appendChild(valueSpan);
+
+        infoContainer.appendChild(div);
+    }
+
+    container.appendChild(infoContainer);
+
+    closeButton = document.createElement('div');
+    closeButton.classList.add('close');
+    closeButton.innerText = '✗';
+    // closeButton.innerHTML = '&Cross;';
+    closeButton.title = 'Close Result';
+    closeButton.addEventListener('click', e => {
+        e.target.parentNode.remove();
+        if(document.querySelector(`div#results`).children.length == 0) {
+        document.querySelector(`#results-title`).style.display = 'none';
+        }
+    });
+    container.appendChild(closeButton);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.innerText = 'Download Wave';
+    link.target = '_blank';
+    link.download = `output-${nowstring.replaceAll('-', '').replace('T', '-').replaceAll(':', '').substr(0,15)}.wav`;
+    container.appendChild(link);
+
+
+    const videoDiv = document.createElement('div');
+    videoDiv.classList.add('video');
+
+    const video = document.createElement('video');
+    video.src = url;
+    video.controls = true;
+
+    videoDiv.appendChild(video);
+    container.appendChild(videoDiv);
+
+    document.querySelector(`#results-title`).style.display = 'block';
+
+    // result.appendChild(container);
+    result.insertBefore(container, result.firstChild);
+
+    hideLoading();
+
+    } catch(e) {
+    console.error('TTS failed:', e);
+    showError(`${e.name}: ${e.message}`);
+    hideLoading();
+    return;
+    }
+}
+
+function populateModelSelector() {
+    const select = document.querySelector(`select#model`);
+    for(const [name, config] of Object.entries(modelConfig)) {
+    option = document.createElement('option');
+    option.value = name;
+    const p = name.split('--');
+    option.innerText = [p[0], p[1], p.slice(2).join('--')].join('/') + (config.languages && config.languages.length > 1 ? ` [${config.languages.join(', ')}]` : '');
+    select.appendChild(option);
+    }
+    modelChanged();
+}
+
+function populateDownloadModelSelector() {
+    const select = document.querySelector(`select#model-to-download`);
+    for(const model of allModels) {
+    option = document.createElement('option');
+    option.value = model;
+    const p = model.split('--');
+    option.innerText = [p[0], p[1], p.slice(2).join('--')].join('/');
+    select.appendChild(option);
+    }
+}
+
+function addExternalSpeaker(file) {
+    const speakerSelect = document.querySelector(`select#speaker`);
+    const externalSpeakers = document.querySelector(`select#speaker > optgroup#external-speakers`);
+    option = document.createElement('option');
+    option.value = '--' + Object.keys(speakerFiles).length;
+    option.innerText = file.name;
+    speakerFiles[option.value] = file;
+    externalSpeakers.appendChild(option);
+    speakerSelect.value = option.value;
+}
+
+function downloadModelChanged() {
+    const modelName = document.querySelector(`select#model-to-download`).value;
+    const a = document.querySelector(`a#download-model`);
+    a.href = `/models/${modelName}/download`;
+    a.classList.remove('hidden');
+}
+
+function modelChanged() {
+    const modelName = document.querySelector(`select#model`).value;
+    const config = modelConfig[modelName];
+    // languages
+    const languageSelect = document.querySelector(`select#language`);
+    languageSelect.innerHTML = '';
+    console.log('model changed', modelName, 'config', config);
+    for(const language of config.languages) {
+    option = document.createElement('option');
+    option.value = language;
+    option.innerText = language;
+    languageSelect.appendChild(option);
+    }
+    // speakers
+    const modelSpeakers = document.querySelector(`select#speaker > optgroup#model-speakers`);
+    modelSpeakers.innerHTML = '';
+    for(const speaker of config.speakers) {
+    option = document.createElement('option');
+    option.value = speaker;
+    option.innerText = speaker;
+    modelSpeakers.appendChild(option);
+    }
+    if(!config.speakers || config.speakers.length === 0) {
+    option = document.createElement('option');
+    option.value = '';
+    option.innerText = 'default';
+    modelSpeakers.appendChild(option);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    await loadModelInfo();
+    await loadAllModelsList();
+
+    loader = document.querySelector('#loader');
+    errorMessage = document.querySelector('#error');
+
+    document.querySelector('select#model').addEventListener('change', modelChanged);
+    document.querySelector('select#model-to-download').addEventListener('change', downloadModelChanged);
+    document.querySelector('button#generate').addEventListener('click', generate);
+    document.querySelector('button#clear').addEventListener('click', clear);
+    document.querySelector('input#speaker-file-selector').addEventListener('change', speakerFileSelected);
+    document.querySelector('#select-speaker').addEventListener('click', selectSpeaker);
+
+    const shiftEnterGenerate = e => {
+    if(e.shiftKey && e.keyCode == 13) {
+        e.preventDefault();
+        e.stopPropagation();
+        generate();
+    }
+    };
+
+    document.querySelector('textarea#text').addEventListener('keydown', shiftEnterGenerate);
+    document.querySelector('select#speaker').addEventListener('keydown', shiftEnterGenerate);
+    document.querySelector('select#language').addEventListener('keydown', shiftEnterGenerate);
+    document.querySelector('select#model').addEventListener('keydown', shiftEnterGenerate);
+
+    populateModelSelector();
+    populateDownloadModelSelector();
+});
--- server.py
+++ server.py
@@ -22,12 +22,61 @@ from typing import Union
 from enum import Enum
 from collections import defaultdict
 import traceback
+import os 
 
 from fastapi import FastAPI, File, UploadFile, Form, Response, HTTPException
 from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.openapi.utils import get_openapi
+
+from TTS.api import TTS
+
+from tian_voice.tts import (tts_list_models, tts_list_all_models, tts_list_model_languages, tts_list_models_by_language,
+                         TTSWrapper, raw_audio_data_to_wav, InvalidInputException, tts_model_components, tts_list_model_speakers, tts_download, tts_async_download)
+
+# model_names = {}
+# for model_name in tts_list_models():
+#     # model_type, lang, dataset, model = model_name.split('/')
+#     model_type, lang, dataset, model = tts_model_components(model_name)
+#     model_names[f'{model_type}_{lang}_{dataset}_{model}'.replace('-', '_')] = model_name
+# ModelName = Enum('ModelName', model_names)
+AllModelName = Enum('AllModelName', [(model_name, model_name) for model_name in tts_list_all_models()])
+
+
+def refresh_models():
+    global models_by_language
+    global model_languages
+    global language_priority
+    global model_priority
+
+    global model_names
+    global ModelName
+    global languages
+    global Languages
+
+    models_by_language = tts_list_models_by_language()
+    model_langauges = tts_list_model_languages()
+    language_priority = defaultdict(int)
+    model_priority = defaultdict(int)
+
+    for language, models in models_by_language.items():
+        for model in models:
+            model_priority[model] += len(models_by_language[language])
+
+    model_names = list(sorted(model_priority.keys(), key=lambda model: '%02d%s' % (len(model_priority) + 1 - model_priority[model], model), reverse=False))
+
+    ModelName = Enum('ModelName', [(model_name, model_name) for model_name in model_names])
+
+    for language, models in models_by_language.items():
+        for model in models:
+            language_priority[language] += len(model_langauges[model])
+
+    languages = list(sorted(language_priority.keys(), key=lambda lang: '%02d%s' % (100-language_priority[lang], lang), reverse=False))
+
+    Languages = Enum('Languages', [(lang, lang) for lang in languages])
+
+refresh_models()
 
 app = FastAPI(debug=debug)
 
@@ -40,20 +89,54 @@ def enable_cors():
         allow_headers=["*"],
     )
 
+def generate(model_name: ModelName,
+                   text: str,
+                   language: Union[Languages, None],
+                   # language: Union[str, None],
+                   speaker: Union[str, None],
+                   speaker_wav: UploadFile,
+                   download: bool):
 
-# @app.get("/reset")
-# def reset():
-#     print(app.router.routes)
-#     app.router.routes = app.router.routes[0:4]
-#     load_routes('y')
-#     print(app.router.routes)
-#     return ''
+    # return dict(model_name=model_name, text=text, language=language, speaker=speaker, speaker_wav=bool(speaker_wav))
 
-# def load_routes(x='x'):
-#     from typing import Literal
-#     @app.get("/test/{some}")
-#     def download_model(some: Literal["a","b", x]):
-#         return some
+    try:
+        model_name = model_name.value
+        if language:
+            language = language.value
+
+        tts = get_tts(model_name)
+
+        if speaker_wav:
+            speaker_wav = speaker_wav.file
+
+        result = tts(text=text, language=language, speaker=speaker, speaker_wav=speaker_wav)
+
+        # data = [int(0x7fff * sample * 0.4) for sample in result.data]
+        # wav_data = raw_audio_data_to_wav(data, result.sample_rate, int)
+        # wav_data = raw_audio_data_to_wav(result.data, result.sample_rate)
+        wav_data = tts.get_wav(result)
+
+        if download:
+             headers = { 'Content-Disposition': 'attachment; filename=output.wav' }
+        else:
+             headers = {}
+
+        return Response(wav_data, media_type='audio/wav', headers=headers)
+
+    except InvalidInputException as e:
+        if debug:
+            traceback.print_exc()
+        raise HTTPException(status_code=400, detail=str(e))
+
+    except Exception as e:
+        if debug:
+            traceback.print_exc()
+        raise HTTPException(status_code=500, detail=str(e))
+        # return JSONResponse(status_code=500, content=dict(error=str(e)))
+
+def get_tts(model_name):
+    return TTSWrapper(model_name)
+
 
 def load_routes(app):
 
@@ -81,33 +164,33 @@ def load_routes(app):
     def list_model_speakers():
         return tts_list_model_speakers()
 
-    # @app.get("/models/{model_name}/download")
-    # def download_model(model_name: AllModelName):
-    #     model_name = model_name.value
-    #     model_name = '/'.join(tts_model_components(model_name))
-    #     return StreamingResponse(tts_download(model_name, reload), media_type='text/plain')
-        # return get_tts(model_name).download()
+    @app.get("/models/{model_name}/download")
+    def download_model(model_name: AllModelName):
+        model_name = model_name.value
+        model_name = '/'.join(tts_model_components(model_name))
+        return StreamingResponse(tts_download(model_name, reload), media_type='text/plain')
+        return get_tts(model_name).download()
 
     # This blocks the main thread, use threaded version above
-    # @app.get("/models/{model_name}/download-async")
-    # async def async_download_model(model_name: AllModelName):
-    #     model_name = model_name.value
-    #     model_name = '/'.join(tts_model_components(model_name))
-    #     return StreamingResponse(tts_async_download(model_name, reload), media_type='text/plain')
-    #     # return get_tts(model_name).download()
+    @app.get("/models/{model_name}/download-async")
+    async def async_download_model(model_name: AllModelName):
+        model_name = model_name.value
+        model_name = '/'.join(tts_model_components(model_name))
+        return StreamingResponse(tts_async_download(model_name, reload), media_type='text/plain')
+        # return get_tts(model_name).download()
 
-    # @app.get("/models/{model_name}/languages")
-    # def model_languages(model_name: ModelName):
-    #     model_name = model_name.value
-    #     return tts_list_model_languages()[model_name]
+    @app.get("/models/{model_name}/languages")
+    def model_languages(model_name: ModelName):
+        model_name = model_name.value
+        return tts_list_model_languages()[model_name]
 
-    # @app.get("/models/{model_name}/speakers")
-    # def model_speakers(model_name: ModelName):
-    #     model_name = model_name.value
-    #     speakers = tts_list_model_speakers()
-    #     return speakers.get(model_name)
-    #     # tts = get_tts(model_name)
-    #     # return tts.speakers
+    @app.get("/models/{model_name}/speakers")
+    def model_speakers(model_name: ModelName):
+        model_name = model_name.value
+        speakers = tts_list_model_speakers()
+        return speakers.get(model_name)
+        # tts = get_tts(model_name)
+        # return tts.speakers
 
     # @app.post('/models/{model_name}/generate')
     # def model_generate_post(model_name: ModelName,
@@ -121,15 +204,15 @@ def load_routes(app):
     #     return generate(model_name, text, language, speaker, speaker_wav, download)
 
 
-    # @app.get('/models/{model_name}/generate')
-    # def model_generate_get(model_name: ModelName,
-    #                        text: str,
-    #                        language: Union[Languages, None] = None,
-    #                        # language: Union[str, None] = None,
-    #                        speaker: Union[str, None] = None,
-    #                        download: bool = False):
+    @app.get('/models/{model_name}/generate')
+    def model_generate_get(model_name: ModelName,
+                           text: str,
+                           language: Union[Languages, None] = None,
+                           # language: Union[str, None] = None,
+                           speaker: Union[str, None] = None,
+                           download: bool = False):
 
-    #     return generate(model_name, text, language, speaker, None, download)
+        return generate(model_name, text, language, speaker, None, download)
 
 
     # @app.post('/generate')
@@ -144,18 +227,22 @@ def load_routes(app):
     #     return generate(model_name, text, language, speaker, speaker_wav, download)
 
 
-    # @app.get('/generate')
-    # def generate_get(model_name: ModelName,
-    #                        text: str,
-    #                        language: Union[Languages, None] = None,
-    #                        # language: Union[str, None] = None,
-    #                        speaker: Union[str, None] = None,
-    #                        download: bool = False):
+    @app.get('/generate')
+    def generate_get(model_name: ModelName,
+                           text: str,
+                           language: Union[Languages, None] = None,
+                           # language: Union[str, None] = None,
+                           speaker: Union[str, None] = None,
+                           download: bool = False):
 
-    #     return generate(model_name, text, language, speaker, None, download)
+        return generate(model_name, text, language, speaker, None, download)
 
-
-    app.mount("/", StaticFiles(directory="examples", html=True), name="static")
+    static_dir = os.path.join(os.path.dirname(__file__), "examples")
+    # Kiểm tra nếu thư mục tồn tại
+    if not os.path.exists(static_dir):
+        os.makedirs(static_dir)  # Tạo thư mục nếu chưa có
+        
+    app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
 
 
 
@@ -169,9 +256,9 @@ load_routes(app)
 
 def create_openapi_schema(app):
     return get_openapi(
-        title="TTS Demo",
+        title="A Voice Synthesizer Sample Program",
         version="0.0.1",
-        description="Text-To-Speech API",
+        description="Application of machine learning in Voice Synthesizer",
         routes=app.routes,
     )
 
@@ -184,6 +271,12 @@ def openapi():
 app.openapi = openapi
 
 enable_cors()   # TODO: make use of fork instead of spawn
+
+
+# set experiment paths
+import os
+output_path = os.path.dirname(os.path.abspath(__file__))
+dataset_path = os.path.join(output_path, "../VCTK/")
 
 if __name__ == '__main__':
 
